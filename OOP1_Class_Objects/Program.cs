e84fd356366fec061a91ef53fbfd790747348d68
phucnv46 <phucnv46@fpt.edu.vn>
2026-01-17 15:43:02 +0700

    upload

---
+++ OOP1_Class_Objects/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace OOP1_Class_Objects
+{
+    internal class Program
+    {
+
+        // Lap trinh huong doi tuong 
+        // Class - Dinh nghia ve kieu du lieu moi
+
+        /* OOP dua cac mo hinh o ngoai doi vao trong code
+         * 4 tinh chat chinh cua OOP: Dong goi (Encapsulation), Ke thua (Inheritance), Da hinh (Polymorphism), Truu tuong (Abstraction)
+         * Class la khuon mau de tao ra doi tuong (Object)
+            
+            class SinhVien: Ten, Tuoi, DiaChi, MaSV => Thuoc tinh (Properties)
+                            Nghi, Di hoc, Di choi => Phuong thuc (Methods)
+
+            Object: Doi tuong - The hien cua lop (Class)    
+
+
+            Súng: Class
+                - Thuoc tinh: Mau sac, Kich thuoc, Co so, SL dan,
+                - Phuong thuoc: Ban, Nap da
+            - Valdal: Object
+                - Mau sac: Den
+                - Kich thuoc: Nho gon
+                - Co so: 8x
+                - SL dan: 30 vien
+                - Phuong thuc: Ban, Nap da
+                  
+         */
+
+        static void PhuongThuc()
+        {
+            Console.WriteLine("Phuong thuc cua Program");
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+               TenClass tenDoiTuong = new TenClass(){Truyen thuoc tinh};
+             */
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+            Sung valdal = new Sung() { TenSung="Valdal", MauSac="Bạc", SoLuongDan=30 }; //Tao doi tuong valdal tu class Sung
+
+            valdal.Ban();
+            valdal.NapDan(10);
+
+            // Khong satatic, chi truy cap duoc thong qua doi tuong
+
+            // Co static , truy cap truc tiep thong qua ten class
+
+            // Tao 1 class Tuong: HP,Mana,TenTuong,...; Phuong thuc: TanCong, DiChuyen
+            //Tao doi tuong tuong1, tuong2 va goi phuong thuc tren doi tuong do
+        }
+
+
+
+    }
+
+    
+}
---
+++ OOP1_Class_Objects/Sung.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1_Class_Objects
+{
+    internal class Sung
+    {
+        //Thuoc tinh (Properties)
+        // Full property
+
+        // Quyen truy cap: private, public, protected, internal
+        /*
+          private: chi truy cap duoc ben trong class
+          internal: truy cap duoc ben trong cung 1 assembly (project)
+          public: truy cap duoc o moi noi 
+          protected: chi truy cap duoc ben trong class do va class ke thua no
+          
+         */
+
+        string _tenSung; // Khai bao truong thong tin (Field)
+
+        public Sung()
+        {
+        }
+
+        public string TenSung // Cung cap kha nang doc/ghi cho truong thong tin
+        {
+            get { return $"{_tenSung}"; }
+            set { _tenSung = value; }
+        }
+
+        public string MauSac { get; set; }
+
+        public int SoLuongDan { get; set; }
+
+        //Phuong thuc (Methods)
+
+        internal void Ban()
+        {
+            Console.WriteLine($"{TenSung} bắn ra đạn");
+        }
+
+        public void NapDan(int soLuong)
+        {
+            SoLuongDan += soLuong;
+            Console.WriteLine($"{soLuong} viên đạn đã được nạp vào {TenSung}");
+        }
+
+        public Sung(string tenSung, string mauSac, int soLuongDan)
+        {
+            TenSung = tenSung;
+            MauSac = mauSac;
+            SoLuongDan = soLuongDan;
+        }
+
+        //contructor - ham khoi tao
+
+
+
+
+
+    }
+}
