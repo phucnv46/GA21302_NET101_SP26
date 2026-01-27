b81f928b52ceea876f6bef4eb59e22b6154cd037
phucnv46 <phucnv46@fpt.edu.vn>
2026-01-27 14:59:35 +0700

    LinQ

---
+++ NC_LinQ/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+
+namespace NC_LinQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // LinQ : Ngôn ngữ truy vấn tích hợp
+
+            List<int> numbers = new List<int>() { 3, 5, 1, -2, 8, -9 };
+
+            Console.WriteLine("So lon nhat trong mang la {0}", numbers.Max(x => x));
+            // Tim phan tu theo dieu kien
+            int soCanTim = numbers.Last(x => x < 8); // trả về object
+            Console.WriteLine($"Vay so can tim la {soCanTim}");
+
+            List<SinhVien> sinhViens = new List<SinhVien>() {
+                new SinhVien(){MSV=1,HoTen="Sinh Vien1",DiaChi="Ha Noi1"},
+                new SinhVien(){MSV=2,HoTen="Sinh Vien2",DiaChi="Ha Noi2"},
+                new SinhVien(){MSV=3,HoTen="Sinh Vien3",DiaChi="Ha Noi1"},
+                new SinhVien(){MSV=4,HoTen="Sinh Vien4",DiaChi="Ha Noi1"},
+                new SinhVien(){MSV=5,HoTen="Sinh Vien5",DiaChi="Ha Noi2"},
+            };
+
+
+
+            // Tim cac sinh vien co dia chi la hanoi1
+            var dsSinhVienCanTim = sinhViens.Where(sv => sv.DiaChi.Equals("Ha Noi1")).ToList(); // trả về danh sách
+
+
+            dsSinhVienCanTim.Sort((sv1, sv2) => sv2.HoTen.CompareTo(sv1.HoTen) );
+
+            // LinQ foreach
+            dsSinhVienCanTim.ForEach(sv =>
+            Console.WriteLine($"Mã sv: {sv.MSV}, Tên:{sv.HoTen}, Địa chỉ:{sv.DiaChi}"));
+
+            List<string> fruits = new List<string>() { "Táo","Lê", "Bưởi", "Chuối"};
+
+            // Nối chữ
+            string cumDuocNoi = string.Join(",", fruits);
+            Console.WriteLine(cumDuocNoi);
+
+            //
+
+
+        }
+
+    }
+
+    public class SinhVien
+    {
+        public int MSV { get; set; }
+        public string? HoTen { get; set; }
+
+        public string? DiaChi { get; set; }
+    }
+}
---
+++ XuLyNgoaiLe/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace XuLyNgoaiLe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindMax(3, 2, 6, 7.21, 421.2,2);
+
+        }
+
+
+        // Yeu viet 1 ham tim so lon nhat cua cac tham so truyen vao
+        // Viet 1 ham khong gioi han so lunog tham so cung loai
+
+        static void FindMax(params double[] mangSo)
+        {
+            double max = mangSo[0];
+
+            foreach (var so in mangSo)
+            {
+                if (so > max) max = so;
+            }
+
+            Console.WriteLine($"Vay so lon nhat la {max}");
+
+        }
+
+
+    }
+}
